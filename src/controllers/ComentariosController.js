7c6fa602339bc84016c9f9ca15c523a3c644bbe0
G2Yang <yangtrabajos@gmail.com>
2024-06-10 19:41:52 +0200

    Ya hecho casi todo

--- src/controllers/ComentariosController.js
+++ src/controllers/ComentariosController.js
@@ -13,10 +13,50 @@ class ReservaController extends BaseController {
         return carta;
     }
 
+    async getLogin(username, password) {
+        const url = new URL('https://app.nocodb.com/api/v2/tables/m797s95h3ko0scy/records');
+        const params = {
+            where: `(Nombre,eq,${username})~and(Credencial,eq,${password})`
+        };
+
+        // Agregar los parámetros a la URL
+        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            }
+        });
+
+        // Retornar directamente la propiedad list que contiene los registros
+        const data = await response.json();
+        return data.list;
+
+    }
+
     async getComentarios() {
         let menu = await this.getCarta();
         // ens quedem només els que tenene menu=true
         return menu.filter(e => e.Comentario);
+    }
+
+    async createItem(Nombre, Comentario) {
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            },
+            body: JSON.stringify({
+                Nombre: Nombre,
+                Comentario: Comentario
+            })
+        });
+
+        const data = await response.json();
+        return data;
     }
 
 }
